c730fae379cd7f1c92e07a64eec34337e041b427
Prateek Chaurasiya <chandra65@outlook.com>
2020-07-03 23:25:10 +0530

    Started Cloning

--- exclone/main.js
+++ exclone/main.js
@@ -6,13 +6,43 @@ const app = electron.app;
 function createWindow(){
     const win  = new electron.BrowserWindow({
         width:800,
-        height:600
-        // webPreferences: {
-        //     nodeIntegration: true
-        // }
+        height:600,
+        show : false,
+        webPreferences: {
+            nodeIntegration: true
+        }
     })
+
+    win.loadFile('index.html').then(function () {
+        win.maximize();
+        win.show();
+        win.removeMenu();
+        win.webContents.openDevTools();
+    });
 }
 
 
-
+//package => app => desktop => open
 app.whenReady().then(createWindow);
+
+
+//syntax
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+  
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+  
+  // In this file you can include the rest of your app's specific main process
+  // code. You can also put them in separate files and require them here.
